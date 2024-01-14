c414db116a9c307cb091206348bf87a99008d9e7
HHChandar20 <HHChandar20@codingburgas.bg>
2024-01-14 18:42:21 +0200

    Add home page

--- GoTaxi/GoTaxi.PL/Controllers/HomeController.cs
+++ GoTaxi/GoTaxi.PL/Controllers/HomeController.cs
@@ -19,6 +19,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Login()
+    {
+        return View();
+    }
+
     public IActionResult Register()
     {
         return View();
