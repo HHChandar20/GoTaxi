8530e5fcf53863c35b949a67f3adf798e9fbaa2f
HHChandar20 <HHChandar20@codingburgas.bg>
2024-01-14 16:38:07 +0200

    Add three layer architecture

---
+++ GoTaxi/GoTaxi.BLL/Services/TaxiServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoTaxi.BLL.Services
+{
+    internal class TaxiServices
+    {
+    }
+}
